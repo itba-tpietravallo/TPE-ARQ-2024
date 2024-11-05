093b6aa0d3123a6de248d55f2897ba6e21040c62
Tomás Pietravallo <hello@tomaspietravallo.com>
2024-11-05 11:31:50 -0300

    chore: fix macros and strcasecmp

--- Userland/libc/string.c
+++ Userland/libc/string.c
@@ -24,7 +24,7 @@ int strcmp(char * str1, char * str2) {
 int strcasecmp(char * str1, char * str2) {
     int i = 0;
     while (str1[i] != 0 && str2[i] != 0) {
-        if (toupper(str1[i]) != toupper(str2[2])) {
+        if (toupper(str1[i]) != toupper(str2[i])) {
             return str1[i] - str2[i];
         }
         i++;
